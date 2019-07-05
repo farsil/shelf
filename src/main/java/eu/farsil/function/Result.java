12e6cf5c62ca76410c7f8d6d963d75f28b3b6cee
Marco Buzzanca <marco.buzzanca@accenture.com>
2019-07-05 18:12:28 +0200

    Added documentation to the Result class

--- src/main/java/eu/farsil/function/Result.java
+++ src/main/java/eu/farsil/function/Result.java
@@ -2,6 +2,37 @@ package eu.farsil.function;
 
 import java.util.function.Supplier;
 
+/**
+ * Represents the outcome of a computation that returns a result.
+ * <p/>
+ * Its fluent interface allows users to chain method calls handling
+ * successful results, and it is the preferred way of using this
+ * class:
+ * <pre>{@code final int result = Try.get(() -> Integer.parseInt(someString))
+ *      .ifSuccessful(i -> System.out.println("Converted value: " + i))
+ *      .map(Math::abs)
+ *      .orElse(0);}</pre>
+ * For completeness's sake, it is also possible, but discouraged, to evaluate
+ * the
+ * success of the computation using
+ * the {@link #isSuccessful()} and the
+ * {@link #getCause()} method and manually extract the value:
+ * <pre>{@code int result;
+ * final Result<Integer> outcome = Try.get(() -> Integer.parseInt(someString));
+ * if (outcome.isSuccessful()) {
+ *      final int converted = outcome.orElseThrow();
+ *      System.out.println("Converted value: " + converted);
+ *      result = Math.abs(converted);
+ * } else {
+ *      // you may access the cause of the exception with getCause() if needed
+ *      result = 0;
+ * }}</pre>
+ * Given the verbosity of the snippet above, it is recommended to use a {@code
+ * try/catch} block.
+ *
+ * @param <T> the type of the result.
+ * @author Marco Buzzanca
+ */
 public interface Result<T> {
 	static <V> Result<V> failure(final Exception cause) {
 		return new FailedResult<>(cause);
@@ -18,11 +49,13 @@ public interface Result<T> {
 	Result<T> flatRecover(
 			final ThrowingFunction<? super Exception, Result<T>> mapper);
 
-	Exception getCause();
-
 	Result<T> ifSuccessful(final ThrowingConsumer<? super T> action);
 
-	boolean isSuccessful();
+	default boolean isSuccessful() {
+		return getCause() == null;
+	}
+
+	Exception getCause();
 
 	<V> Result<V> map(final ThrowingFunction<? super T, ? extends V> mapper);
 
