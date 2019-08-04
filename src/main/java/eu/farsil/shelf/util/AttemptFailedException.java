86d214f5260593266d94dbb17d4b937141d26d46
Marco Buzzanca <marco.bzn@gmail.com>
2019-08-04 15:51:54 +0200

    Do not expose AttemptFailedException
    Users are not supposed to instantiate this exception, nor catch it.

--- src/main/java/eu/farsil/shelf/util/AttemptFailedException.java
+++ src/main/java/eu/farsil/shelf/util/AttemptFailedException.java
@@ -1,11 +1,7 @@
 package eu.farsil.shelf.util;
 
-import org.apiguardian.api.API;
-
 import java.io.Serializable;
 import java.util.Objects;
-
-import static org.apiguardian.api.API.Status.MAINTAINED;
 
 /**
  * Exception that is thrown when the method {@link Try#orElseThrow()} is
@@ -13,8 +9,8 @@ import static org.apiguardian.api.API.Status.MAINTAINED;
  *
  * @author Marco Buzzanca
  */
-@API(status = MAINTAINED, since = "0.1.0")
-public class AttemptFailedException extends RuntimeException {
+// package-private, users are not supposed to use or catch this class
+class AttemptFailedException extends RuntimeException {
 	/**
 	 * Required by {@link Serializable}.
 	 */
@@ -26,7 +22,6 @@ public class AttemptFailedException extends RuntimeException {
 	 * @param cause the cause of the failure.
 	 * @throws NullPointerException if the cause is {@code null}.
 	 */
-	// package-private, users are not supposed to instantiate this class
 	AttemptFailedException(final Throwable cause) {
 		super(Objects.requireNonNull(cause));
 	}
